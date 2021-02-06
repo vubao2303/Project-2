cfe09d845f6e4b5ab449e0ea8a7d68638f575e9b
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-05 16:21:07 -0800

    Create empty route functions, to be filled once model is completed

--- routes/api-route.js
+++ routes/api-route.js
@@ -7,20 +7,20 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/signin", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/newuser", function(req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
       .then(function() {
-        res.redirect(307, "/api/login");
+        res.redirect(307, "/api/signin");
       })
       .catch(function(err) {
         res.status(401).json(err);
@@ -44,9 +44,20 @@ module.exports = function(app) {
 };
 
 // Create route to attend a new party ; PUT request
+app.put("/api/attend/:id"), function (req,res){
 
+}
 
 // Create route to create a new party ; POST request
+app.post("/api/newparty"), function (req,res){
+
+}
+
+// Create find parties route
+app.get("/api/findparties") , function (req,res){
+
+}
+
 
 
 
