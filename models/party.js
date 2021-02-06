6e704a6fc843b3f35ce1dc843a339de794067b6d
Coleman Buffa <75283364+coleman-buffa@users.noreply.github.com>
2021-02-06 12:12:14 -0800

    Update party.js

--- models/party.js
+++ models/party.js
@@ -1,71 +1,43 @@
-// module.exports = function (sequelize, DataTypes) {
-// 	let Party = sequelize.define("Party", {
-// 		title: {
-// 			type: DataTypes.STRING,
-// 			allowNull: false
-// 		},
-// 		theme: {
-// 			type: DataTypes.STRING,
-// 			allowNull: false
-// 		},
-// 		date: {
-// 			type: DataTypes.DATE,
-// 			allowNull: false,
-// 			validate: {
-// 				isDate: true
-// 			}
-// 		},
-// 		time: {
-// 			type: DataTypes.TIME,
-// 			allowNull: false,
-// 			validate: {
-// 				isTime: true
-// 			}
-// 		},
-// 		location: {
-// 			type: DataTypes.STRING,
-// 			allowNull: false
-// 		}
-
-// 	});
-// 	Party.associate = models => {
-// 		//Each party belongs to a single User
-// 		models.Party.belongsTo(models.User, { as: "host" })
-// 		//Each Party can have many UserParties
-// 		models.Event.hasMany(models.UserParties, {
-// 			onDelete: "cascade"
-// 		})
-// 		}
-// 	return Party;
-// };
-
-
-// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-module.exports = function (sequelize, DataTypes) {
-  var Event = sequelize.define("Event", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
+ module.exports = function (sequelize, DataTypes) {
+ 	let Party = sequelize.define("Party", {
+ 		title: {
+ 			type: DataTypes.STRING,
+ 			allowNull: false,
+        validate: {
+          len: [1]
       }
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }
-  });
+ 		},
+ 		theme: {
+ 			type: DataTypes.STRING,
+ 			allowNull: false
+ 		},
+ 		date: {
+ 			type: DataTypes.DATE,
+ 			allowNull: false,
+ 			validate: {
+ 				isDate: true
+ 			}
+ 		},
+ 		time: {
+ 			type: DataTypes.TIME,
+ 			allowNull: false,
+ 			validate: {
+ 				isTime: true
+ 			}
+ 		},
+ 		location: {
+ 			type: DataTypes.STRING,
+ 			allowNull: false
+ 		}
 
-  // associating Events table to Users and UserEvents
-  Event.associate = models => {
-    // Each Event belongs to A User
-    models.Event.belongsTo(models.User, { as: "host" })
-    // Each Event can have many UserEvents
-    models.Event.hasMany(models.UserEvent, {
-      // this deletes all associated UserEvents when an Event is deleted
-      onDelete: "cascade"
-    })
-  }
-  return Event;
-}
+ 	});
+ 	Party.associate = models => {
+ 		//Each party belongs to a single User
+ 		models.Party.belongsTo(models.User, { as: "host" })
+ 		//Each Party can have many UserParties
+ 		models.Party.hasMany(models.UserParties, {
+ 			onDelete: "cascade"
+ 		})
+ 		}
+ 	return Party;
+ };
