7114b378821b32656d480789928956fd7078376e
BtramVu <bbbvut@gmail.com>
2021-02-07 01:59:25 -0800

    add click here tag to switch between log in page and sign up page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@
      res.sendFile(path.join(__dirname, "../public/htmls/signup.html"));
    });
 
-   app.get("/signin", function(req, res) {
+   app.get("/login", function(req, res) {
      // If the user already has an account send them to the members page
      if (req.user) {
        res.redirect("/dashboard");
